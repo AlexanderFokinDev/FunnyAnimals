3ae24c71fff18586fad29c33e7afe3e82942f77d
Aleksandr Fokin <alexanderfokindev@gmail.com>
2022-08-16 11:07:16 -0300

    Initial commit

---
+++ app/src/main/java/uy/amn/funnyanimals/MainActivity.kt
@@ -0,0 +1,43 @@
+package uy.amn.funnyanimals
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import uy.amn.funnyanimals.ui.theme.FunnyAnimalsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FunnyAnimalsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FunnyAnimalsTheme {
+        Greeting("Android")
+    }
+}
